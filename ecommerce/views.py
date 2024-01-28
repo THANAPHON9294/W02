528b49549c9b793a204255a118b52af2911590bc
THANAPHON9294 <toey.170246@gmail.com>
2024-01-28 23:09:52 +0700

    Add another page & Routing page

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -14,3 +14,11 @@ def item_view(request, item_id):
         }
     
     return render(request, 'index.html',context= context_data)
+
+def page_view(request, page_name):
+
+    context_data = {
+        "page_name": page_name
+    }
+
+    return render(request, f'{page_name}.html', context= context_data)
